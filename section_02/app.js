b94fef6a9f525063c65e44220ecdfc77f764639b
Davi Silva Santos <morg.daviss@gmail.com>
2023-01-05 22:11:32 -0300

    .

--- section_02/app.js
+++ section_02/app.js
@@ -7,4 +7,14 @@ function add(n1, n2) {
 function printResult(num) {
     console.log('The result is: ' + num);
 }
+var trash;
 printResult(add(5, 7));
+console.log(printResult(add(5, 7)));
+console.log(' ');
+console.log("lesson 27 function as types");
+console.log(' ');
+var combineValues;
+// let combineValues: Function;
+//combineValues = 1 -> error
+combineValues = add;
+console.log(combineValues(7, 9));
--- section_02/app.ts
+++ section_02/app.ts
@@ -10,4 +10,24 @@ function printResult(num: number){
     console.log('The result is: ' + num)
 }
 
+let trash: undefined;
+
 printResult(add(5, 7))
+console.log(printResult(add(5, 7)))
+
+console.log(' ')
+console.log("lesson 27 function as types")
+console.log(' ')
+
+let combineValues: (a:number, b: number) => number;
+
+// let combineValues: Function;
+//combineValues = 1 -> error
+
+combineValues = add; 
+
+console.log(combineValues(7, 9))
+
+console.log(' ')
+console.log("lesson 28 function types & callbacks")
+console.log(' ')
